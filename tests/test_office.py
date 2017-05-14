3277da28eac543c10c4fc59a85543378d7735dd5
wikedzit <timothy.wikedzi@andela.com>
2017-05-14 07:39:09 +0300

    Implemented the first test for Office  class

---
+++ tests/test_office.py
@@ -0,0 +1,11 @@
+import unittest
+
+class OfficeTest(unittest.TestCase):
+    """docstring for OfficeTest"""
+    def test_create_room(self):
+        room = Room()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
