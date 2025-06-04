0879d758c82c1565e139eb6e074ea1a76928e945
Timothy Wikedzi <twikedzi@carnegielearning.com>
2025-06-04 09:59:14 +0300

    Fix tests and improve documentation

--- tests/test_office.py
+++ tests/test_office.py
@@ -2,29 +2,36 @@ import unittest
 
 
 class Office(object):
-    """docstring for Office"""
+    """Simple representation of an office."""
+
     _capacity = 6
-    def __init__(self,details):
-        self.name = details['name']
+
+    def __init__(self, details=None):
+        details = details or {"name": ""}
+        self.name = details["name"]
+
+    def add(self, name):
+        if not isinstance(name, str):
+            raise TypeError("Office name must be a string")
+        self.name = name
+        return self.save()
 
     def save(self):
-        if  True:
-            return True
-        else:
-            return False
+        return True
 
-    def edit(self,office_id,data):
-        if type(office_id) is not int :
+    def edit(self, office_id, data):
+        if type(office_id) is not int:
             raise TypeError(" ID must be a number")
         office = Office.find(office_id)
-        if isinstance(office,Office):
-            office.name = data['name']
-            if self.save():return True
+        if isinstance(office, Office):
+            office.name = data["name"]
+            if self.save():
+                return True
         return False
-
+    @staticmethod
     def find(office_id):
-        if office_id is 1:
-            return Office()
+        if office_id == 1:
+            return Office({"name": "Main"})
         return None
 
 #--------------------------------------------------------------------------------------
@@ -50,9 +57,8 @@ class TestOffice(unittest.TestCase):
 
     def test_edit_office(self):
 
-        self.office.edit(1,{'name':'Office'})
-
-        self.assertTrue()
+        result = self.office.edit(1, {"name": "Office"})
+        self.assertTrue(result)
 
 
 if __name__ == '__main__':
